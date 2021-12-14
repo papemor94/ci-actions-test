6ecb9636b9414fb787a934ba577e6ec5d3b447a9
pape <papemor94@gmail.com>
2021-12-14 12:02:57 +0100

    set user and paszds

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -35,6 +35,6 @@ public class Main {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-        System.out.println(   "hello github actions 3 ");
+        System.out.println(   "hello github actions 34");
     }
 }
